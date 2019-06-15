52ec3c6139660c997a721fc9d8bbd68201269096
Maxime <maxime.cappellen@live.fr>
2019-06-15 21:41:03 +0200

    New controller statique

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'statique/accueil'
+
+  get 'statique/contact'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root "application#premier"
