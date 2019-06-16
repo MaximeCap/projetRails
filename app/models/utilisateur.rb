8446aa6dbfc37e1ec98c1fe9fc0871cb8a7342e8
Maxime <maxime.cappellen@live.fr>
2019-06-17 01:48:11 +0200

    Ajout des validations pour les utilisateurs

--- app/models/utilisateur.rb
+++ app/models/utilisateur.rb
@@ -1,2 +1,9 @@
 class Utilisateur < ApplicationRecord
+	before_save {self.email = email.downcase}
+
+		validates :nom, presence: true, length: { maximum: 50 }
+		VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\Z/i
+		validates :email, presence: true, length: { maximum: 244 },
+						format: { with: VALID_EMAIL_REGEX },
+						uniqueness: {case_sensitive:  false}
 end
---
+++ db/migrate/20190616234401_ajout_index_email_user.rb
@@ -0,0 +1,5 @@
+class AjoutIndexEmailUser < ActiveRecord::Migration[5.1]
+  def change
+    add_index :utilisateurs, :email, unique: true
+  end
+end
