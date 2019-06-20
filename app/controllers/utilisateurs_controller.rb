6153e049a90199bf5635a2e50e18e033aac94787
Maxime <maxime.cappellen@live.fr>
2019-06-21 00:15:54 +0200

    Ajout des messages d'erreurs | Close #6

--- app/controllers/utilisateurs_controller.rb
+++ app/controllers/utilisateurs_controller.rb
@@ -6,4 +6,20 @@ class UtilisateursController < ApplicationController
   def show
     @utilisateur = Utilisateur.find(params[:id])
   end
+
+  def create
+    @utilisateur = Utilisateur.new(utilisateur_params)# On crée une autre fonction car si on utilise params[] > permet à n'importe qui d'avoir des droits administrateurs
+    if @utilisateur.save
+      # Sauvegarde confirmé <- True
+    else
+      render "new"
+    end
+  end
+
+  private 
+
+  def utilisateur_params
+    params.require(:utilisateur).permit(:nom, :email, :password, :password_confirmation)
+  end
+
 end
